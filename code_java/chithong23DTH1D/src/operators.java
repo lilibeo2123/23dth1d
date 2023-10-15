7a67824b6b1f9a01ba6a58bc829f404b9ef28683
lilibeo2123 <141105322+lilibeo2123@users.noreply.github.com>
2023-10-16 00:54:48 +0700

    bài tập 23DTH1D

---
+++ code_java/chithong23DTH1D/src/operators.java
@@ -0,0 +1,16 @@
+public class operators {
+   public static void main(String[] args) 
+   {
+
+    int x = 5, y = 8;
+    //concatenates x and y as
+    //firts x is added to "concatenation (x+y) = "
+    //producing "concatenation (x+y) = 5"
+    //and then 8 is further concatenated.
+    System.out.println("concatenation (x+y)= " + x + y );
+
+    //addition of x and y
+    System.out.println("addition (x+y) = " + (x + y));
+   }
+}
+   
